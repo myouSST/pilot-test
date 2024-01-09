7dba10349d40a322cfa6cd455272411ed293ffe1
유민 <wkzkfdl1@gmail.com>
2024-01-09 18:40:34 +0900

    샘플 도메인 버그 수정

--- src/main/java/com/example/copilottest/domain/User.java
+++ src/main/java/com/example/copilottest/domain/User.java
@@ -1,17 +1,18 @@
 package com.example.copilottest.domain;
 
 import com.example.copilottest.domain.organization.Organization;
-import com.example.copilottest.domain.organization.OrganizationType;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @EqualsAndHashCode
+@ToString
 public class User {
 
     private String id;
@@ -26,7 +27,7 @@ public class User {
         return new User("myou", "유민", "http://test-profile.co.kr", Organization.sample());
     }
 
-    public void isTechOrganization() {
-        return organization.has(OrganizationType.TECH);
+    public static void main(String[] args) {
+        System.out.println(sample());
     }
 }
